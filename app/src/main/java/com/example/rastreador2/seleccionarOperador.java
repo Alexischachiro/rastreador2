a7815b17ef20f8b7cee554288c3f8f77a49c2df3
Martin Melo <nitram-210397@hotmail.com>
2019-10-30 08:46:19 -0600

    Added onClick on ListView

--- app/src/main/java/com/example/rastreador2/seleccionarOperador.java
+++ app/src/main/java/com/example/rastreador2/seleccionarOperador.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import com.example.rastreador2.entidades.Usuario;
 import com.example.rastreador2.repositories.usuarioRepo;
@@ -17,6 +21,9 @@ public class seleccionarOperador extends AppCompatActivity {
     Button btnselecherra;
     ListView listViewUsuarios;
     ArrayList<String> listaUsuarios = new ArrayList<>();
+    ArrayList<Usuario> usuarios = null;
+    Usuario userSelected;
+    TextView txtUserSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +39,9 @@ public class seleccionarOperador extends AppCompatActivity {
             }
         });
         listViewUsuarios = findViewById(R.id.listViewUsers);
+        txtUserSelected = findViewById(R.id.txtUserSelected);
         usuarioRepo repo = new usuarioRepo(this);
-        ArrayList<Usuario> usuarios = repo.getAll();
+        usuarios = repo.getAll();
         this.getUserLists(usuarios);
 
         ArrayAdapter adapter = new ArrayAdapter(
@@ -42,6 +50,17 @@ public class seleccionarOperador extends AppCompatActivity {
                 listaUsuarios
         );
         listViewUsuarios.setAdapter(adapter);
+
+        listViewUsuarios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.d("ONCLICK", "item " + listViewUsuarios.getItemAtPosition(position));
+                Log.d("ONCLICK", "position " + position );
+                Log.d("ONCLICK", "id " + id);
+                Log.d("ONCLICK", "view " + view.toString());
+                setSelectedUser(position);
+            }
+        });
     }
 
     private void getUserLists(ArrayList<Usuario> usuarios) {
@@ -52,6 +71,11 @@ public class seleccionarOperador extends AppCompatActivity {
         } else {
             listaUsuarios.add("No existen usuarios registrados");
         }
+    }
+
+    private void setSelectedUser(int position) {
+        userSelected = usuarios.get(position);
+        txtUserSelected.setText(userSelected.getNombre());
     }
 
 }
