2b6b7087594e4587d2e4bb0c81fc23e3649b69ef
Chachiro <duvan_chachiro@hotmail.com>
2019-11-11 18:52:16 -0600

    Se agregaron dos activitys para el estatus del operador

---
+++ app/src/main/java/com/example/rastreador2/detallesOperadorHerrameintas.java
@@ -0,0 +1,18 @@
+package com.example.rastreador2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class detallesOperadorHerrameintas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_operador_herrameintas);
+    }
+
+    public void onClick(View view) {
+    }
+}
